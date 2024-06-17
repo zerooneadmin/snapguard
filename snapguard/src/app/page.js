80bff028ef03507ba0b08045244636f808521327
SahanMedonsa <87310977+SahanMedonsa@users.noreply.github.com>
2024-06-17 12:45:12 +0530

    snapguard home

---
+++ snapguard/src/app/page.js
@@ -0,0 +1,11 @@
+import { Component } from "@/components/component";
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <Component></Component>
+      </div>
+    </main>
+  );
+}
